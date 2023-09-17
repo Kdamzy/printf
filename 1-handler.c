aeb95e6069de796debb03fb9aebcd604d4653d7e
Adeyemi0804 <abdulazeezadeyinka0804@gmail.com>
2023-09-17 15:18:19 +0100

    ytt

--- 1-handler.c
+++ 1-handler.c
@@ -15,26 +15,48 @@
 
 
 int print_char(va_list ap)
-{       
-        return _putchar(va_arg(ap, int));
+{
+	return _putchar(va_arg(ap, int));
 }
+
+
+/**
+ * print_string - printsa  string of characters.
+ * @ap: A variable argument list containing the string to be printed.
+ *
+ * Description: This function prints each character in the string
+ * using the _putchar function
+ *
+ * Return: The number of characters printed
+ */
+
 
 int print_string(va_list ap)
 {
 	char *str = va_arg(ap, char *);
 	int count = 0;
 
-	while (*str + count) != '\0')
+	while (*(str + count) != '\0')
 	{
 		_putchar(*str + count));
-		count ++;
+		count++;
 	}
-	return count;
+
+	return (count);
 }
+/* print_digit - prints an integer digit.
+ * @ap: A variable argument list containing the integer to be printed.
+ *
+ * Description: This function takes an integer as a variable
+ * argument and prints it using the 'put_int' function.
+ *
+ * Return: The number of characters printed by 'put_int'.
+ *
+ */
+
 
 int print_digit(va_list ap)
 {
 	int num = va_arg(ap, int);
 	return put_int(num);
 }
-
