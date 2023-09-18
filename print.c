2df1e6c4d94f715b31791995c6f8996f44e8cc74
Kdamzy <kolajidedamilare@gmail.com>
2023-09-18 07:30:17 -0700

    initial commit

--- print.c
+++ print.c
@@ -46,6 +46,11 @@ int loop(va_list ap, char *p)
 		{'s', print_string},
 		{'d', print_digit},
 		{'i', print_digit},
+		{'b', print_binary},
+		{'u', print_unsign},
+		{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_hex_upper},
 		{'\0', NULL}
 	};
 	int count = 0, i = 0;
