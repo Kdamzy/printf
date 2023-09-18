3d917b1381beba7f132b696b5c67874bacca2ff1
Adeyemi0804 <abdulazeezadeyinka0804@gmail.com>
2023-09-18 09:31:47 +0100

    yu

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int len;
+
+	len = _printf("Hello, %s !\n", "world");
+	_printf("Length: [%d]\n", len);
+
+	len = _printf("Character: [%c]\n", 'A');
+	_printf("Length: [%d]\n", len);
+
+	len = _printf("Integer: [%d]\n,", 12345);
+	_printf("Length: [%d]\n", len);
+
+	len = _printf("Hexadecimal: [%x]\n", 255);
+	_printf("Length: [%d]\n", len);
+
+	len = _printf("Percent: [%%]\n");
+	_printf("Length: [%d]\n", len);
+
+	return (0);
+}
